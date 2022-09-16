eb4edaefd8c5ffba1ed4a390d9327570cdd4f0ee
Sergey <serega_krotov90@mail.ru>
2022-09-16 22:48:08 +0500

    vetvlenie_1

---
+++ Vetvlenie_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедити имя:   ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это маша");
+}
+else
+{
+    Console.Write("Привет  ");
+    Console.WriteLine(username);
+}
